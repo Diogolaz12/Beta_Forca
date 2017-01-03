6e76c33dfcac3a2a9dd98896bc8b37c4a13269e7
Adraythien <davidbentogeral@hotmail.com>
2017-01-03 13:50:43 +0000

    alguns metodos

--- src/main.java
+++ src/main.java
@@ -1,21 +1,32 @@
+import java.util.Random;
 
 public class main {
 
+	String[] listaPalavras = {"Software","Engenharia","Linux", "Processador","RAM","Penacova"};
+	String palavra;
+	Random gerarpalavra = new Random();
+	String palavra1;
 	public static void main(String[] args) {
 		
 		
-		String[] listaPalavras = 
-			{"PETROLEO", "PESTANA", "PESTILENTO", "PETELECO", "REBOQUE", "CADAFALSO",
-			"CADEIRA", "COLA", "REBENTO", "DEFUMADO", "DISCURSO", "ELETRODOMESTICO",
-			"ELETRONICA", "ENGRENAGEM", "ESFOMEADO", "FERRALHEIRO", "FERROVIA", 
-			"FERTIL", "FORTALEZA", "FORTIFICANTE", "OFICINA", "ORNAMENTO", "PALAVRA",
-			"PREOCUPACAO", "RADIOLOGIA", "RADIOGRAFIA", "GRANJA", "GRANULADO", "INDUZIDO",
-			"IMBATIVEL", "INDUSTRIA", "INTERNACIONAL", "LABIRINTO", "LOBISOMEM",
-			"LOCOMOTIVA", "TESOURA", "MASSAGISTA", "MATADOURO", "MOCHILA", "NOROESTE",
-			"NITROGLICERINA", "HELICOPTERO", "CAPITALISMO", "SOFTWARE", "ENGENHARIA","LINUX", "PROCESSADOR"};
-		
-		
-
 	}
-
+	
+	public void Verificar(){
+		
+		if( palavra1.contains(palavra)){
+			
+			
+			
+			
+		}
+	}
+	
+	
+	public String Gerar(){
+				
+		int palavragerada= gerarpalavra.nextInt(listaPalavras.length);
+		palavra1=listaPalavras[palavragerada];
+		return palavra1;
+	}
+	
 }
